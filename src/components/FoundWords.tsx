b5c1f4468f4e5f4e15d29feb26dd33a45a213ff3
Matthew Cole <matthewrichardcole@gmail.com>
2021-08-25 13:19:06 -0600

    style FoundWords component

--- src/components/FoundWords.tsx
+++ src/components/FoundWords.tsx
@@ -2,15 +2,20 @@ import * as R from 'ramda'
 
 import { orderByWordScore, ScoreType, scoreWord } from "../game";
 
+import './FoundWords.css'
+
 export type FoundWordsProps = { foundWords: string[], scoreType: ScoreType }
 
 const FoundWords: React.FC<FoundWordsProps> = ({ foundWords, scoreType }) => {
   const orderedWords = orderByWordScore(foundWords, scoreType)
-  const makeFoundWord = (word: string) => <div className="found-word-container">
-    <div className="word">{word}</div>
-    <div className="score">+{scoreWord(word, scoreType)}</div>
-  </div>
-  return <div className="found-words-container">{R.map(makeFoundWord, orderedWords)}</div>
+
+  const makeFoundWord = (word: string) =>  <div className="found-words-word">{word.toUpperCase()}</div> 
+  const makeScore = (word: string) => <div className="found-words-score">+{scoreWord(word, scoreType)}</div>
+
+  return <div className="found-words-container">
+    <div className="found-words-words">{R.map(makeFoundWord, orderedWords)}</div>
+    <div className="found-words-scores">{R.map(makeScore, orderedWords)}</div>
+    </div>
 }
 
 export default FoundWords
