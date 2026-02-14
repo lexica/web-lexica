b4ad663f4801af8baa0753f11378a705389c56b1
Matthew Cole <matthewrichardcole@gmail.com>
2026-02-13 22:29:16 -0700

    Several small fixes - Simplify the component structure for game screen - Remove useCss hooks from qrcode - Remove unused js files - Fix local development environment needing /web-lexica

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -9,14 +9,7 @@ type Colors = {
 
 export type QrCodeProps = {
   info: string,
-  size?: number,
   colors?: Colors
-}
-
-const applySize = (size: number, svg: string) => {
-  const regex = /(width|height)="[\d]+px"/g
-
-  return svg.replace(regex, `$1="${size}px"`)
 }
 
 const applyColor = ({ foreground, background }: Colors, svg: string) => {
@@ -26,22 +19,12 @@ const applyColor = ({ foreground, background }: Colors, svg: string) => {
   return svg.replace(foregroundRx, `fill="${foreground}"`).replace(backgroundRx, `fill="${background}"`)
 }
 
-const applyProps = ({ size, colors }: { size?: number, colors?: Colors}, svg: string) => {
-  if (!size && !colors) return svg
-
-  if(!size) return applyColor(colors!, svg)
-
-  if(!colors) return applySize(size!, svg)
-
-  return applyColor(colors, applySize(size, svg))
-}
-
 const QrCode = (props: QrCodeProps): JSX.Element => {
   const { info } = props
   const qr = Qr(0, 'M')
   qr.addData(info)
   qr.make()
-  const svg = applyProps(props, qr.createSvgTag())
+  const svg = props.colors ? applyColor(props.colors, qr.createSvgTag()) : qr.createSvgTag()
 
   return <div className="qr-code" dangerouslySetInnerHTML={{ __html: svg }}/>
 }
--- src/pages/SinglePlayer.tsx
+++
@@ -1,5 +0,0 @@
-import GameScreen from './GameScreen'
-
-const SinglePlayer = (): JSX.Element => <GameScreen isNewGame />
-
-export default SinglePlayer
